bd94c1820df976d5426e8893050e301c9a29c0ae
namkellbi <namkellbi@gmail.com>
2023-08-20 16:40:38 +0700

    initial project

---
+++ src/main/java/nam/fpt/mediamarketing/MediaMarketingApplication.java
@@ -0,0 +1,13 @@
+package nam.fpt.mediamarketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaMarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaMarketingApplication.class, args);
+	}
+
+}
